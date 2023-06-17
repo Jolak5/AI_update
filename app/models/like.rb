e963fc68483d9ad9a2ed61c594393d7a33722d14
jolak5 <kayodej4@gmail.com>
2023-06-17 19:12:43 +0100

    Fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
- 
-  
-  
+
+
+
   after_create :update_likes_counter
 
   private
